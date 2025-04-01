0621bd832d9f3b003538e07c1a85684f64299e41
Reshord <daribayev1982@bk.ru>
2025-04-01 19:03:09 +0500

    Добавлены модели и админка для управления ресторанами, обновлены зависимости

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Restaurant
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'display_iiko_status', 'id')
+    list_filter = ('city',)
+    search_fields = ('name', 'city__name', 'iiko_organization_id')
+    readonly_fields = ('get_created', 'get_modified')
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'city')
+        }),
+        ('Интеграция с iiko', {
+            'fields': ('iiko_organization_id', 'external_menu_id', 'price_category_id', 'department_id'),
+            'classes': ('collapse',),
+            'description': 'Настройки для интеграции с системой iiko'
+        }),
+    )
+
+    def display_iiko_status(self, obj):
+        if obj.iiko_organization_id:
+            return format_html('<span style="color:green;">✓</span>')
+        return format_html('<span style="color:red;">✕</span>')
+    display_iiko_status.short_description = 'iiko'
+
+    def get_created(self, obj):
+        return getattr(obj, 'created_at', 'Нет данных')
+    get_created.short_description = 'Дата создания'
+
+    def get_modified(self, obj):
+        return getattr(obj, 'modified_at', 'Нет данных')
+    get_modified.short_description = 'Последнее изменение'
+
+    actions = ['mark_no_iiko']
+    
+    def mark_no_iiko(self, request, queryset):
+        queryset.update(iiko_organization_id=None, external_menu_id=None, price_category_id=None, department_id=None)
+        self.message_user(request, f"Очищены данные интеграции iiko для {queryset.count()} ресторанов.")
+    mark_no_iiko.short_description = "Очистить данные интеграции с iiko"
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('city')
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+import uuid
+import qrcode
+from io import BytesIO
+from PIL import Image
 
-# Create your models here.
+from django.db import models
+from django.core.files import File
+from django.conf import settings
+from cities.models import City  
+
+class Restaurant(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название ресторана'
+    )
+
+    city = models.ForeignKey(
+        City, 
+        on_delete=models.CASCADE,
+        related_name='restaurants',
+        verbose_name='Город'
+    )
+
+    iiko_organization_id = models.CharField(
+        max_length=255,
+        verbose_name='ID организации из iiko Cloud API',
+        null=True,
+        blank=True
+    )
+    
+    external_menu_id = models.CharField(
+        max_length=255,
+        verbose_name='Внешний ID меню из iiko Cloud API',
+        null=True,
+        blank=True
+    )
+
+    price_category_id = models.CharField(
+        max_length=255,
+        verbose_name='ID ценной категории из iiko Cloud API',
+        null=True,
+        blank=True
+    )
+
+    department_id = models.CharField(
+        max_length=255,
+        verbose_name='ID подразделения из iiko Waiter',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Ресторан'
+        verbose_name_plural = 'Рестораны'
+        indexes = [models.Index(fields=['name'])]
