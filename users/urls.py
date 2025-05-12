4bc04be4ed9f10cd20d9302cdb6a7ccd89fe073a
Reshord <daribayev1982@bk.ru>
2025-05-13 01:48:12 +0500

    add verify

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView, 
 )
 
 from .views import UserViewSet, UserRegistrationView, ProfileViewSet
@@ -18,6 +19,7 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include(profile_router.urls)),  # URL для профиля
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
+    path('', include(profile_router.urls)), 
     path('', include(router.urls)),
 ]
